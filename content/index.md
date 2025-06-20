68a0ad49f9143b1cdcb87aeae27dda7549c7917b
Niels Bekkema <nielsbekkema@MacBookAir.home>
2025-06-20 13:29:00 +0200

    Quartz sync: Jun 20, 2025, 1:29 PM

--- content/index.md
+++ content/index.md
@@ -1,6 +1,7 @@
 ---
-title: Welcome to Quartz
+title:
 ---
 
-This is a blank Quartz installation.
-See the [documentation](https://quartz.jzhao.xyz) for how to get started.
+this is a website in progress.
+
+[[Note two]]
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Niels Bekkema",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -23,13 +23,13 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Inter",
+        body: "Inter",
         code: "IBM Plex Mono",
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
+          light: "#fffcf0",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
